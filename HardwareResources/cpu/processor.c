2b7789b29f9c3fe232ed0d0a8fc7061aef517090
AdrianoTosetto <adriano.rafael10@hotmail.com>
2017-02-11 02:31:14 -0200

    .

--- HardwareResources/cpu/processor.c
+++ HardwareResources/cpu/processor.c
@@ -12,11 +12,11 @@ instructionFormat getInstructionFormat(uint32_t instruction) {
 		return I_TYPE;
 }
 instructionType getInstructioType(uint32_t instruction) {
-	instructionFormat t = getInstructionFormat(instruction);
+	instructionFormat f = getInstructionFormat(instruction);
 
-	switch(t) {
+	switch(f) {
 		case R_TYPE:
-
+			
 		break;
 		case J_TYPE:
 
@@ -28,17 +28,94 @@ instructionType getInstructioType(uint32_t instruction) {
 }
 
 
-uint32_t getOpcode(uint32_t instruction){}
-uint32_t getRT(uint32_t instruction){}
-uint32_t getRD(uint32_t instruction){}
-uint32_t getRS(uint32_t instruction){}
-uint32_t getInstrConstant(uint32_t instruction){}
-uint32_t getFunct(uint32_t instruction){}
-uint32_t getShamt(uint32_t instruction){}
+uint32_t getOpcode(uint32_t instruction) {
+	return (instruction & 0xFC000000) >> 26;
+}
+uint32_t getRS(uint32_t instruction) {
+
+	return (instruction & 0x03D00000) >> 21;
+}
+uint32_t getRT(uint32_t instruction) {
+	return (instruction & 0x001F0000) >> 16; //1111 11 11 111 1 1111 1111
+}
+uint32_t getRD(uint32_t instruction){
+	return (instruction & 0x0000F800) >> 11;
+}
+uint32_t getInstrConstant(uint32_t instruction){
+	return (instruction & 0x0000FFFF);
+}
+uint32_t getFunct(uint32_t instruction){
+	return (instruction & 0x0000003F);
+}
+uint32_t getShamt(uint32_t instruction){
+	return (instruction & 0x000007C0) >> 6;
+}
+
+/*
+* Mips uses the funct field to differentiate the R-type instructions. 
+*
+*/
+instructionType getInstructioTypeFormatR(uint32_t instruction) {
+	uint32_t functField = getFunct(instruction);
+	switch(functField) {
+		case 0x20:
+			return ADD;
+		break;
+		case 0x21:
+			return ADDU;
+		break;
+		case 0x24:
+			return AND;
+		break;
+		case 0x1A:
+			return DIV;
+		break;
+		case 0x1B:
+			return DIVU;
+		break;
+		case 0x08:
+			return JR;
+		break;
+		case 0x18:
+			return MULT;
+		break;
+		case 0x19:
+			return MULTU;
+		break;
+		case 0x27:
+			return NOR;
+		break;
+		case 0x26:
+			return XOR;
+		break;
+		case 0x25:
+			return OR;
+		break;
+		case 0x2A:
+			return SLT;
+		break;
+		case 0x00:
+			return SLL;
+		break;
+		case 0x02:
+			return SRL;
+		break;
+		case 0x22:
+			return SUB;
+		break;
+		case 0x23:
+			return SUBU;
+		break;
+		default:
+
+		return _WHAT_;
+	}
+}
+instructionType getInstructioTypeFormatI(uint32_t instruction);
+instructionType getInstructioTypeFormatJ(uint32_t instruction);
 
 int main(int argc, char const *argv[])
 {
-	printf("%d\n",getInstructionFormat(0x00622020));
-	printf("%d\n", getInstructionFormat(0b10001100110001010000000000001000));
+	printf("%d\n",getInstructioTypeFormatR(0b00000011000000000111100000100000));
 	return 0;
 }
